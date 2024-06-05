96b913e6c636bf10adc7fd9b8f3bc249f0fd8ff9
dvtate <toast27@gmail.com>
2024-06-04 21:58:46 -0500

    oops

--- cmake-build-release/third_party/seastar/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tate/Desktop/git-fed/third_party/seastar
-# Build directory: /home/tate/Desktop/git-fed/cmake-build-release/third_party/seastar
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
